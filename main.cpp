ae4f15850177c6ee4f1f037f7ba1604ae7bf0d50
joshuamango <ajoshuaodeyemi@gmail.com>
2025-05-03 20:37:29 -0400

    Finish lexer

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ enum Token {
 static std::string IdentifierStr; // Filled in if tok_identifier
 static double NumVal;             // Filled in if tok_number
 
-
 // gettok - Return the next token from standard input
 static int gettok() {
   static int LastChar = ' ';
@@ -25,6 +24,54 @@ static int gettok() {
   while (isspace(LastChar))
     LastChar = getchar();
 
-  return 0;
+  // Handle identifiers
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  // Handle numbers
+  if (isdigit(LastChar) || LastChar == '.') {
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF) {
+      return gettok();
+    }
+
+  }
+
+  if (LastChar == EOF) {
+    return tok_eof;
+  }
+
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+int main() {
+  int tok;
+  while ((tok = gettok()) != '\n') {
+    std::cout << tok << std::endl;
+  }
 }
 
